80fc767a245e833b5fce1c670d3e5e2f1490ffe9
ShindeViraj <shindeviraj895@gmail.com>
2026-06-22 11:59:17 +0530

    Fix cross-origin dev resources access

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ['192.168.0.26', 'localhost', '127.0.0.1'],
+  },
 };
 
 export default nextConfig;
